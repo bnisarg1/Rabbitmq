fdf2eeb9556ef6989767476dbea309630e6e1598
Nisarg <bnisarg1@gmail.com>
2019-11-25 00:23:08 +0530

    Springboot Rabbit Mq

---
+++ src/test/java/com/example/messagingrabbitmq/MessagingRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.messagingrabbitmq;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class MessagingRabbitmqApplicationTests {
+
+	///@Test
+	void contextLoads() {
+	}
+
+}
